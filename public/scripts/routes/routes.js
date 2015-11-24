419dee53bb912ae3d850a27e4510bfe4c0f787db
Jack Sheriff <jsheriff@mitre.org>
2015-11-24 13:22:36 -0500

    Use manual injection for the router.

--- public/scripts/routes/routes.js
+++ public/scripts/routes/routes.js
@@ -2,38 +2,40 @@
 
 angular
     .module('meshweshApp')
-    .config(['$routeProvider',
-        function($routeProvider) {
-            $routeProvider
-                .when('/home', {
-                    templateUrl:'views/home.html',
-                    controller:'HomeController'
-                })
-                .when('/thematicCategories/list', {
-                    templateUrl:'views/thematicCategoriesList.html',
-                    controller:'ThematicCategoriesListController',
-                    controllerAs: 'vm'
-                })
-                .when('/thematicCategories/:categoryId/edit', {
-                    templateUrl:'views/thematicCategoriesEdit.html',
-                    controller:'ThematicCategoriesEditController',
-                    controllerAs: 'vm'
-                })
-                .when('/thematicCategories/new', {
-                    templateUrl:'views/thematicCategoriesEdit.html',
-                    controller:'ThematicCategoriesEditController',
-                    controllerAs: 'vm'
-                })
-                .when('/report', {
-                    templateUrl:'report.html',
-                    controller:'ReportCtrl'
-                })
-                .when('/export', {
-                    templateUrl:'export.html',
-                    controller:'ExportCtrl'
-                })
-                .otherwise({
-                    redirectTo: '/home'
-                });
+    .config(router);
 
-        }]);
+router.$inject = ['$routeProvider'];
+
+function router($routeProvider) {
+    $routeProvider
+        .when('/home', {
+            templateUrl:'views/home.html',
+            controller:'HomeController'
+        })
+        .when('/thematicCategories/list', {
+            templateUrl:'views/thematicCategoriesList.html',
+            controller:'ThematicCategoriesListController',
+            controllerAs: 'vm'
+        })
+        .when('/thematicCategories/:categoryId/edit', {
+            templateUrl:'views/thematicCategoriesEdit.html',
+            controller:'ThematicCategoriesEditController',
+            controllerAs: 'vm'
+        })
+        .when('/thematicCategories/new', {
+            templateUrl:'views/thematicCategoriesEdit.html',
+            controller:'ThematicCategoriesEditController',
+            controllerAs: 'vm'
+        })
+        .when('/report', {
+            templateUrl:'report.html',
+            controller:'ReportCtrl'
+        })
+        .when('/export', {
+            templateUrl:'export.html',
+            controller:'ExportCtrl'
+        })
+        .otherwise({
+            redirectTo: '/home'
+        });
+}
