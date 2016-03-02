24254d07688663e3514a11e441517d95673e631c
Jack Sheriff <jsheriff@mitre.org>
2016-03-02 10:45:16 -0500

    Move date range check to controller.

--- public/scripts/controllers/modals/dateRangeEditController.js
+++ public/scripts/controllers/modals/dateRangeEditController.js
@@ -7,6 +7,48 @@ angular
 function DateRangeEditController($uibModalInstance, dateRange) {
     var vm = this;
 
+    vm.expandedDateRange = expandDateRange(dateRange);
+
+    vm.setStartPeriod = function(period) {
+        vm.expandedDateRange.startDatePeriod = period;
+        vm.dateChanged();
+    };
+
+    vm.setEndPeriod = function(period) {
+        vm.expandedDateRange.endDatePeriod = period;
+        vm.dateChanged();
+    };
+
+    vm.ok = function () {
+        var updatedDateRange = unexpandDateRange(vm.expandedDateRange);
+        $uibModalInstance.close(updatedDateRange);
+    };
+
+    vm.cancel = function () {
+        $uibModalInstance.dismiss('cancel');
+    };
+
+    vm.dateChanged = function() {
+        vm.editDateRangeForm.inputEndDateYear.$setValidity("dateOrder", checkDateOrder(vm.expandedDateRange));
+    }
+}
+
+function checkDateOrder(expandedDateRange) {
+    var dateRange = unexpandDateRange(expandedDateRange);
+    console.log("check: " + dateRange.startDate + " " + dateRange.endDate);
+    if (dateRange.startDate && dateRange.endDate) {
+        return (dateRange.endDate > dateRange.startDate);
+    }
+    else {
+        // Always true if not able to compare numbers
+        // This allows another validation message to be displayed
+        return true;
+    }
+}
+
+function expandDateRange(dateRange) {
+    // Create the expanded date range
+    //   -100 becomes { 100, "BC" }
     var startPeriod = "AD";
     if (dateRange.startDate < 0) {
         startPeriod = "BC";
@@ -17,41 +59,29 @@ function DateRangeEditController($uibModalInstance, dateRange) {
         endPeriod = "BC";
     }
 
-    vm.expandedDateRange = {
+    var expandedDateRange = {
         startDateYear: Math.abs(dateRange.startDate),
         startDatePeriod: startPeriod,
         endDateYear:  Math.abs(dateRange.endDate),
         endDatePeriod: endPeriod
     };
 
-    vm.setStartPeriod = function(period) {
-        vm.expandedDateRange.startDatePeriod = period;
-    };
-
-    vm.setEndPeriod = function(period) {
-        vm.expandedDateRange.endDatePeriod = period;
-    };
-
-    vm.ok = function () {
-        var updatedDateRange = {
-            startDate: vm.expandedDateRange.startDateYear,
-            endDate: vm.expandedDateRange.endDateYear
-        };
-
-        if (vm.expandedDateRange.startDatePeriod === "BC") {
-            updatedDateRange.startDate = updatedDateRange.startDate * -1;
-        }
-
-        if (vm.expandedDateRange.endDatePeriod === "BC") {
-            updatedDateRange.endDate = updatedDateRange.endDate * -1;
-        }
-
-        $uibModalInstance.close(updatedDateRange);
-    };
-
-    vm.cancel = function () {
-        $uibModalInstance.dismiss('cancel');
-    };
-
+    return expandedDateRange;
 }
 
+function unexpandDateRange(expandedDateRange) {
+    var dateRange = {
+        startDate: expandedDateRange.startDateYear,
+        endDate: expandedDateRange.endDateYear
+    };
+
+    if (expandedDateRange.startDatePeriod === "BC") {
+        dateRange.startDate = dateRange.startDate * -1;
+    }
+
+    if (expandedDateRange.endDatePeriod === "BC") {
+        dateRange.endDate = dateRange.endDate * -1;
+    }
+
+    return dateRange;
+}
