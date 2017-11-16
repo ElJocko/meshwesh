078229014d153974bfed8494d21c261346bb0b6e
Jack Sheriff <jsheriff@mitre.org>
2017-11-16 17:26:36 -0500

    Update to node 8.

--- run-server-test.sh
+++ run-server-test.sh
@@ -1,7 +1,11 @@
 #!/bin/bash
 
+printf "\n*** Setting correct version of npm. ***\n\n"
+. ~/.nvm/nvm.sh
+nvm use
+
 printf "\n*** Restoring database to starting state. ***\n\n"
 mongoimport --db meshwesh-test --collection thematiccategories --drop --file test/data/thematic-categories.json
 
 printf "\n*** Starting API test. ***\n\n"
-grunt test-server
+./node_modules/.bin/grunt test-server
--- start-test-server.sh
+++ start-test-server.sh
@@ -1,3 +1,8 @@
 #!/bin/bash
 
+printf "\n*** Setting correct version of npm. ***\n\n"
+. ~/.nvm/nvm.sh
+nvm use
+
+printf "\n*** Starting server. ***\n\n"
 node ./bin/start-test.js
